26dad5f0f785efb6f05033cfba16f2d9f7afd9e5
koremayur <Mayurkore4@gmail.com>
2025-04-04 00:56:00 -0400

    groceryTotal

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -8,3 +8,16 @@ function calculateArea() {
 let area = length * width;
  document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+let grocery1;
+let grocery2;
+let grocery3;
+
+function calculateTotalAmount() {
+    grocery1 = parseFloat(document.getElementById('grocery-1').value);
+    grocery2 = parseFloat(document.getElementById('grocery-2').value);
+    grocery3 = parseFloat(document.getElementById('grocery-3').value);
+
+    let totalAmount = grocery1 + grocery2 + grocery3
+    document.getElementById('totalamount').innerHTML = `The total amount is: $ ${totalAmount}`;
+}
